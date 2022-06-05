905589d0d01641d191caca20709267cbaedc6f7f
dhanika9025 <101445290+dhanika9025@users.noreply.github.com>
2022-06-05 10:11:17 +0530

    Add files via upload

---
+++ cowbul.py
@@ -0,0 +1,114 @@
+
+import random
+import random
+def game():
+    print("hello dear welcome to my game ______")
+    name=input("Hello dear enter your name_______")
+    print("welcome dear",name)
+    num=set()
+    i=1
+    while i<=5:
+        num.add(random.randint(0,9))
+        if len(num)!=5:
+            num.add(random.randint(0,9))
+        if len(num)==5:
+            break
+        i=i+1
+    list1=list(num)
+    print(list1)
+    
+    j=0
+    chance=9
+    cow=0
+    bull=0
+    li2=[]
+    li3=[]
+    while 1<=chance:
+        user_choice=int(input("enter the guess number__"))
+        user_position=int(input("enter the position__"))
+        li3.append(user_position)
+        print("aapke pass bs ",chance,"chances hi bche h_____")
+        if user_choice in list1:
+            li2.append(user_choice)  
+            if li3[j]==list1.index(user_choice):
+                bull+=1
+                print(bull,"bull")
+            else:
+                cow+=1
+                print(cow,"cow")
+        if len(li2)==len(list1)==len(li3)==5:
+            break
+        chance-=1
+        j+=1
+    print(li2)
+    print(li3)
+    if cow<=1 :
+        print("losser")
+    else:
+        print("* congratulations",name, "you have* ",bull,"bulls")
+        print("dear",name,"aap jit gye")
+    play=input("agr aap dobara khelna chahte ho to : yes/no me se ek dalo___")
+    if play=="yes":
+        game()
+    else:
+        return "exit , jab aapka mn kre tb khel lena   "
+    print("hello dear welcome to my game ______")
+    name=input("Hello dear enter your name_______")
+    print("welcome dear",name)
+    num=set()
+    i=1
+    while i<=5:
+        num.add(random.randint(0,9))
+        if len(num)!=5:
+            num.add(random.randint(0,9))
+        if len(num)==5:
+            break
+        i=i+1
+    list1=list(num)
+    print(list1)
+    
+    j=0
+    chance=9
+    cow=0
+    bull=0
+    li2=[]
+    li3=[]
+    while 1<=chance:
+        user_choice=int(input("enter the guess number__"))
+        user_position=int(input("enter the position__"))
+        li3.append(user_position)
+        print("aapke pass bs ",chance,"chances hi bche h_____")
+        if user_choice in list1:
+            li2.append(user_choice)  
+            if li3[j]==list1.index(user_choice):
+                bull+=1
+                print(bull,"bull")
+            else:
+                cow+=1
+                print(cow,"cow")
+        if len(li2)==len(list1)==len(li3)==5:
+            break
+        chance-=1
+        j+=1
+    print(li2)
+    print(li3)
+    if cow<=1 :
+        print("losser")
+    else:
+        print("* congratulations",name, "you have* ",bull,"bulls")
+        print("dear",name,"aap jit gye")
+    play=input("agr aap dobara khelna chahte ho to : yes/no me se ek dalo___")
+    if play=="yes":
+        game()
+    else:
+        return "exit , jab aapka mn kre tb khel lena   "
+print(game())
+    
+
+
+    
+
+
+    
+
+
